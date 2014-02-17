6285c6563054dba3a277afdabdeb632abb108336
unknown <troky2001@yahoo.com>
2014-02-17 09:14:11 +0100

    Fixed scrypt_1024_1_1_256_sp() for better cross-platform compatibility

--- scrypt.c
+++ scrypt.c
@@ -356,7 +356,10 @@ salsa20_8(uint32_t B[16], const uint32_t Bx[16])
 /* cpu and memory intensive function to transform a 80 byte buffer into a 32 byte output
    scratchpad size needs to be at least 63 + (128 * r * p) + (256 * r + 64) + (128 * r * N) bytes
  */
-static void scrypt_1024_1_1_256_sp(const uint32_t* input, char* scratchpad, uint32_t *ostate, const cl_uint n = 1024)
+
+#define scrypt_1024_1_1_256_sp(input, scratchpad, ostate)  scrypt_n_1_1_256_sp(input, scratchpad, ostate, 1024)
+
+static void scrypt_n_1_1_256_sp(const uint32_t* input, char* scratchpad, uint32_t *ostate, const cl_uint n)
 {
 	uint32_t * V;
 	uint32_t X[32];
